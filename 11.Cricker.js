6dcbdf3f8c52a1cefc18e7d9fe632514b5f1b2c8
Likith Kumar S <101611090+Skllit@users.noreply.github.com>
2025-07-16 17:56:03 +0530

    Create 11.Cricket.js

---
+++ 11.Cricker.js
@@ -0,0 +1,118 @@
+
+
+const allPlayers = [...playersData];
+
+export const getPlayers = () => {
+return allPlayers;
+};
+
+export const generateId = () => {
+const maxId = Math.max(...allPlayers.map(p => p.id), 0);
+return maxId + 1;
+};
+
+export const getPlayer = (id) => {
+return allPlayers.find(p => p.id === id);
+};
+
+export const addPlayer = (player) => {
+allPlayers.push(player);
+};
+
+export const updatePlayer = (index, newPlayer) => {
+allPlayers[index] = newPlayer;
+};
+
+
+export const deletePlayer = (index) => {
+allPlayers.splice(index, 1);
+};
+
+export const getIndexOfPlayer = (id) => {
+return allPlayers.findIndex(p => p.id === id);
+};
+-------------------------------------------------------------
+
+const alert = (msg) => {
+toast.error(msg, {position: 'top-right'});
+
+}
+
+let team = {
+title: "Mumbai Indians",
+players: [],
+};
+
+export const getTeam = () => {
+return team;
+
+};
+
+export const setTeam = (tm) => {
+team = tm;
+
+};
+
+
+export const editTeamPlayer = (id, nwPlayer) => {
+
+ const index = team.players.findIndex(p => p.id === id);
+  if(index !== -1){
+   team.players[index] = nwPlayer;
+ }
+};
+
+
+export const addTeamPlayer = (player) => {
+const exists = team.players.find(p => p.id === player.id);
+ if(exists){
+alert("This Player already exist in your team !!");
+return;
+}
+team.players.push(player);
+};
+
+
+
+export const deleteTeamPlayer = (id) => {
+team.players = team.players.filter(p => p.id !== id);
+};
+
+
+
+export const getStatus = () => {
+const batsmen = team.players.filter(p => p.role === 'Batsman').length;
+const bowlers = team.players.filter(p => p.role === 'Bowler').length;
+const keepers = team.players.filter(p => p.role === 'Wicket Keeper').length;
+
+ const errors = [];
+ if(batsmen !== 2){
+   errors.push('You do not have required number of batsmen in your team');
+   }
+
+   if(bowlers < 2){
+   errors.push('You do not have required number of bowler in your team');
+   }
+
+   if(keepers < 1){
+   errors.push('You do not have the required number of wicketkeepers in your team');
+    }
+
+ return errors;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
